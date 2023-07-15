3828fd3981d3a96e3ecf226a07f292f2592fb445
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2023-07-15 21:08:52 +0530

    Added solution - LeetHub

--- Replace all 0's with 5 - GFG/replace-all-0s-with-5.cpp
+++ Replace all 0's with 5 - GFG/replace-all-0s-with-5.cpp
@@ -1,43 +1,36 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
- 
+
+int convertFive(int n);
+
 // Driver program to test above function
-
-// } Driver Code Ends
-class Solution{
-  public:
-    /*you are required to complete this method*/
-    int convertFive(int n)
-    {
-        int ans=0,rem=0,i=0;
-        while(n!=0){
-            rem=n%10;
-            if(rem==0){
-                rem=5;
-                ans=ans+rem*pow(10,i);
-            }
-            else{
-                ans=ans+rem*pow(10,i);
-            }
-            i++;
-            n/=10;
-        }
-        return ans;
-    }
-};
-
-//{ Driver Code Starts.
-int main()
-{
+int main() {
     int T;
-    cin>>T;
-    while(T--)
-    {
-    	int n;
-    	cin>>n;
-    	Solution obj;
-    	cout<<obj.convertFive(n)<<endl;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        cout << convertFive(n) << endl;
     }
 }
 // } Driver Code Ends
+
+
+/*you are required to complete this method*/
+int convertFive(int n) {
+    int ans=0,rem=0,i=0;
+    while(n!=0){
+        rem=n%10;
+        if(rem==0){
+            rem=5;
+            ans=ans+rem*pow(10,i);
+        }
+        else{
+            ans=ans+rem*pow(10,i);
+        }
+        i++;
+        n/=10;
+    }
+    return ans;
+}
