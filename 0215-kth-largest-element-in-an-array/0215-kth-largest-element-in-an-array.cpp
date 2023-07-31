a189e5a2df0765d98ec0b25ab8fb1f99ab024825
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2023-07-31 20:34:08 +0530

    Time: 252 ms (5.32%), Space: 45.5 MB (85.07%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int partition(vector<int> &arr, int s, int e) {
+
+    int pivot = arr[s];
+
+    int cnt = 0;
+    for(int i = s+1; i<=e; i++) {
+        if(arr[i] <=pivot) {
+            cnt++;
+        }
+    }
+
+    //place pivot at right position
+    int pivotIndex = s + cnt;
+    swap(arr[pivotIndex], arr[s]);
+
+    //left and right wala part smbhal lete h 
+    int i = s, j = e;
+
+    while(i < pivotIndex && j > pivotIndex) {
+
+        while(arr[i] <= pivot) 
+        {
+            i++;
+        }
+        while(arr[j] > pivot) {
+            j--;
+        }
+        if(i < pivotIndex && j > pivotIndex) {
+            swap(arr[i++], arr[j--]);
+        }
+    }
+    return pivotIndex;
+
+}
+
+void quickSort(vector<int> &arr, int s, int e) {
+    //base case
+    if(s >= e){
+        return;
+    } 
+
+    //partitioon karenfe
+    int p = partition(arr, s, e);
+
+    //left part sort karo
+    quickSort(arr, s, p-1);
+
+    //right wala part sort karo
+    quickSort(arr, p+1, e);
+
+}
+    int findKthLargest(vector<int>& nums, int k) {
+        int n=nums.size();
+        quickSort(nums, 0, n-1);
+        return nums[n-k];
+    }
+};
