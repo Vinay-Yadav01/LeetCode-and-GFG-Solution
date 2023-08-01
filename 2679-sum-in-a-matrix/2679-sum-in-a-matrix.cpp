e7fcd1ef87fd54feee386871e3c6c3693825243b
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2023-08-01 19:18:55 +0530

    Time: 92 ms (99.68%), Space: 47.8 MB (95.51%) - LeetHub

--- 2679-sum-in-a-matrix/2679-sum-in-a-matrix.cpp
+++ 2679-sum-in-a-matrix/2679-sum-in-a-matrix.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    int maxi(vector<vector<int>>& nums,int size,int row){
-        int maximum=0,index=0;
-        for(int i=0;i<size;i++){
-            if(nums[row][i]>maximum) {
-                maximum = nums[row][i];
-                index=i;
-            }
-        }
-        nums[row][index]=0;
-        return maximum;
-    }
     int matrixSum(vector<vector<int>>& nums) {
-        int ans=0;
-        int n = nums.size();
-        int size = nums[0].size();
-        for(int i=0;i<size;i++){
-            int maximum=0;
-            for(int j=0;j<n;j++){
-                int maxRow=maxi(nums,size,j);
-                if(maximum < maxRow ) maximum = maxRow;                
-            }
-            ans+=maximum;
+       int n = nums.size();
+        int m = nums[0].size();
+
+        int score = 0;
+
+        for (int i = 0; i < n; i++){
+            sort(nums[i].begin(), nums[i].end(), greater<int>());
         }
-        return ans;
+
+        for (int j = 0; j < m; j++){
+            int maxi = nums[0][j];
+            for (int i = 0; i < n; i++){
+                maxi = max(maxi, nums[i][j]);
+            }
+            score += maxi;
+        }
+
+        return score; 
     }
 };
