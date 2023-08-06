a4306411af005b842220474c4eec23585b232305
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2023-08-06 20:25:55 +0530

    Time: 7 ms (72.36%), Space: 14.7 MB (52.79%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,35 +10,52 @@
  */
 class Solution {
 public:
+    ListNode* solve(ListNode* first, ListNode* second) {
+        if(first->next==NULL){
+            first->next=second;
+            return first;
+        }
+        ListNode* curr1 = first;
+        ListNode* next1 = curr1 -> next;   
+        ListNode* curr2 = second;
+        ListNode* next2 = curr2 -> next;
+
+        while(next1 != NULL && curr2 != NULL) {       
+            if( (curr2 -> val >= curr1 -> val ) 
+               && ( curr2 -> val <= next1 -> val)) {          
+                curr1 -> next = curr2;
+                next2=curr2->next;
+                curr2 -> next = next1;
+                curr1 = curr2;
+                curr2 = next2;
+            }
+            else {  
+                curr1=next1;
+                next1=next1->next;
+                if(next1==NULL){
+                    curr1->next=curr2;
+                    return first;
+                }         
+            }        
+        }
+        return first;
+    }
+
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
        
-		// if list1 happen to be NULL
-		// we will simply return list2.
-		if(l1 == NULL)
+        if(l1 == NULL)
+            return l2;
+
+        if(l2 == NULL)
+            return l1;
+
+        if(l1 -> val <= l2 -> val ){
+             return solve(l1, l2);
+        }
+        else
         {
-			return l2;
-		}
-		
-		// if list2 happen to be NULL
-		// we will simply return list1.
-		if(l2 == NULL)
-        {
-			return l1;
-		} 
-		
-		// if value pointend by l1 pointer is less than equal to value pointed by l2 pointer
-		// we wall call recursively l1 -> next and whole l2 list.
-		if(l1 -> val <= l2 -> val)
-        {
-			l1 -> next = mergeTwoLists(l1 -> next, l2);
-			return l1;
-		}
-		// we will call recursive l1 whole list and l2 -> next
-		else
-        {
-			l2 -> next = mergeTwoLists(l1, l2 -> next);
-			return l2;            
-		}
-	}
+            return solve(l2, l1);
+        }
+    }
 };	
    
