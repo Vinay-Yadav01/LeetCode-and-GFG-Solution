300514c26723b6bed7d8860cd1e54c0178cfca51
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-06-04 20:17:56 +0530

    Time: 0 ms (100%), Space: 7.6 MB (32.43%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,23 +3,19 @@ public:
     bool isValid(string s) {
         stack<char> st;
         for(int i=0;i<s.size();i++){
-            char ch=s[i];
+            char ch = s[i];
             if(ch=='(' || ch=='{' || ch=='['){
                 st.push(ch);
             }
             else{
-                if(!st.empty()){
-                    char element=st.top();
-                    if((element=='('&& ch==')')||( element=='{' && ch=='}')||( element=='[' && ch==']')){
-                        st.pop();
-                    }
-                    else return false;
+                if(st.empty()) return false;
+                if ((ch==')' && st.top()=='(')|| (ch=='}' && st.top()=='{') ||(ch==']' && st.top()=='[')){
+                    st.pop();
                 }
                 else return false;
             }
         }
-
-        if(st.empty()) return true;
-        else return false;                   
+        if(st.size()==0) return true;
+        else return false;
     }
 };
